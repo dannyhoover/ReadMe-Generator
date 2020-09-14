4059f23c3ceb161dc825ea2d466ad1b970672434
dannyhoover <dofjrtd@gmail.com>
2020-09-14 12:12:43 -0700

    Readme generates now just need badge functionality

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,27 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## Testing
+${data.tests}
+
+## Licensing
+${data.license}
+
+### Contact Info
+${data.githubUsername}
+${data.email}
 `;
 }
 
